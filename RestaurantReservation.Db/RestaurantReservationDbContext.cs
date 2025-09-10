6970f36ee39bae8539dc28130ead643b0ec446eb
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-09-11 01:09:28 +0300

    Refactor RestaurantReservationDbContext to include DbSet properties and configure database options

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,6 +1,37 @@
-﻿namespace RestaurantReservation.Db
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db
 {
-    public class RestaurantReservationDbContext
+    public class RestaurantReservationDbContext : DbContext
     {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = RestaurantReservationCore"
+            ).LogTo(Console.WriteLine,
+                    new[] { DbLoggerCategory.Database.Command.Name },
+                    LogLevel.Information)
+            .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity.HasKey(r => r.RestaurantId);
+                entity.Property(r => r.Name).IsRequired();
+                entity.Property(r => r.Address).IsRequired();
+                entity.Property(r => r.PhoneNumber).IsRequired();
+                entity.Property(r => r.OpeningHours).IsRequired();
+            });
+
+            modelBuilder.Entity<MenuItem>().HasKey(menuItem => menuItem.ItemId);
+        }
     }
 }
