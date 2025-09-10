3e3efe655e0b709ba7a0d8e2a8da44eb09535f17
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-09-11 01:52:55 +0300

    Add initial migration and fixed DbContext to include all existing models

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -7,7 +7,13 @@ namespace RestaurantReservation.Db
     public class RestaurantReservationDbContext : DbContext
     {
         public DbSet<Restaurant> Restaurants { get; set; }
-
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Table> Tables { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -22,6 +28,9 @@ namespace RestaurantReservation.Db
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // An issue occurs with Restaurant model in which it can't define 
+            // the primary key on it's own
             modelBuilder.Entity<Restaurant>(entity =>
             {
                 entity.HasKey(r => r.RestaurantId);
@@ -31,7 +40,30 @@ namespace RestaurantReservation.Db
                 entity.Property(r => r.OpeningHours).IsRequired();
             });
 
+            // The primary key in MenuItem model does not follow EF rules
+            // so it has to be defined manually
             modelBuilder.Entity<MenuItem>().HasKey(menuItem => menuItem.ItemId);
+
+            // Orders deleted when Reservation is deleted
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Reservation)
+                .WithMany(r => r.Orders)
+                .HasForeignKey(o => o.ReservationId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // OrderItems deleted when Order is deleted
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // No action for deletion: restaurant -> reservation
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Restaurant)
+                .WithMany(rt => rt.Reservations)
+                .HasForeignKey(r => r.RestaurantId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
